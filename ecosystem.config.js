0b85344f25c74dececd51f4fd50eb70cb1cd6a56
handwithfivefingers <handgod1995@gmail.com>
2022-07-21 23:18:43 +0700

    config ci

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [
+    {
+      name: 'TEST_CI_0.1',
+      script: './App.js',
+      watch: ['build', 'server', 'App.js'],
+      ignore_watch: ['node_modules', 'uploads', 'src'],
+      exec_mode: 'cluster',
+      autorestart: true,
+      max_memory_restart: '1G',
+      error_file: './uploads/logs/error.log',
+      out_file: './uploads/logs/out.log',
+      log_date_format: 'CC_LOGS ::: HH:mm Z ::: DD-MM-YYYY',
+      combine_logs: true,
+    },
+  ],
+};
