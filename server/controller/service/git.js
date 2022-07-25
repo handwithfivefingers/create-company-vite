f9252baaf45d871960a7bde1dc23d0f0feb5b423
OMINEXT\truyenmv <truyenmv@ominext.com>
2022-07-25 11:42:03 +0700

    fix check header git

--- server/controller/service/git.js
+++ server/controller/service/git.js
@@ -6,13 +6,14 @@ const secret = 'Hdme195';
 const repo = '/usr/share/nginx/html/create-company-vite';
 
 const gitAction = async (req, res) => {
-	if (process.env.NODE_ENV === 'development') return;
+	// if (process.env.NODE_ENV === 'development') return res.end();
 
 	try {
 		// let command = 'cd ' + repo + '\ && git checkout -- . \ && git pull';
-		res.end();
 
 		let isValid = await validateGit(req, res);
+
+		res.end();
 
 		if (!isValid) return;
 
@@ -53,16 +54,18 @@ const gitAction = async (req, res) => {
 const validateGit = async (req, res) => {
 	let result = false;
 
-	console.log(req.rawBody);
-	console.log(req.body);
+	const payload = JSON.stringify(req.body);
 
-	if (!req.rawBody) {
+	console.log('payload', payload);
+	if (!payload) {
 		result = false;
 	}
 
 	const sig = new Buffer.from(req.get(sigHeaderName) || '', 'utf8');
-	const hmac = crypto.createHmac(sigHashAlg, secret);
-	const digest = new Buffer.from(sigHashAlg + '=' + hmac.update(req.rawBody).digest('hex'), 'utf8');
+
+	const hmac = crypto.createHmac(sigHashAlg, secret)
+
+	const digest = new Buffer.from(sigHashAlg + '=' + hmac.update(payload).digest('hex'), 'utf8');
 
 	if (sig.length !== digest.length || !crypto.timingSafeEqual(digest, sig)) {
 		console.log(`Request body digest (${digest}) did not match ${sigHeaderName} (${sig})`);
