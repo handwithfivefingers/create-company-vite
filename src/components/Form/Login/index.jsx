e7cf9fa0c5477a934cf35dc2f008fa945b9e247a
handwithfivefingers <handgod1995@gmail.com>
2022-08-04 01:02:10 +0700

    add Google login

--- src/components/Form/Login/index.jsx
+++ src/components/Form/Login/index.jsx
@@ -1,11 +1,12 @@
-import { Button, Form, Input, Space, Spin } from "antd";
-import clsx from "clsx";
-import React, { forwardRef } from "react";
-import styles from "./Login.module.scss";
+import { Button, Form, Input, Space, Spin } from 'antd'
+import clsx from 'clsx'
+import React, { forwardRef } from 'react'
+import { FcGoogle } from 'react-icons/fc'
+import styles from './Login.module.scss'
 
 const LoginForm = forwardRef((props, ref) => {
   return (
-    <div className={clsx([styles.loginWrap, "container"])}>
+    <div className={clsx([styles.loginWrap, 'container'])}>
       <h1>Đăng nhập</h1>
       <Spin spinning={props.loading}>
         <Form ref={ref} onFinish={props.onFinish} layout="vertical">
@@ -15,15 +16,20 @@ const LoginForm = forwardRef((props, ref) => {
           <Form.Item name="password" label="Mật khẩu">
             <Input.Password />
           </Form.Item>
-          <Space style={{ display: "flex", justifyContent: "space-between" }}>
+          <Space style={{ display: 'flex', justifyContent: 'space-between' }}>
             <Button type="primary" htmlType="submit">
               Đăng nhập
             </Button>
+            <Button
+              type="ghost"
+              icon={<FcGoogle style={{ fontSize: 24 }} />}
+              onClick={props?.loginWithGoogle}
+            />
           </Space>
         </Form>
       </Spin>
     </div>
-  );
-});
+  )
+})
 
-export default LoginForm;
+export default LoginForm
--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,62 +1,69 @@
-import React, { useContext, useEffect, useRef, useState } from "react";
-import LoginForm from "@/components/Form/Login";
-import RegisterForm from "@/components/Form/Register";
-import { useNavigate, useLocation, useNavigationType } from "react-router-dom";
+import React, { useContext, useEffect, useRef, useState } from 'react'
+import LoginForm from '@/components/Form/Login'
+import RegisterForm from '@/components/Form/Register'
+import { useNavigate, useLocation, useNavigationType } from 'react-router-dom'
 
-import { Tabs } from "antd";
-import AuthService from "@/service/AuthService";
-import { AuthAction } from "@/store/actions";
-import { useDispatch, useSelector } from "react-redux";
-import RouterContext from "@/helper/Context";
+import { Tabs } from 'antd'
+import AuthService from '@/service/AuthService'
+import { AuthAction } from '@/store/actions'
+import { useDispatch, useSelector } from 'react-redux'
+import RouterContext from '@/helper/Context'
 
-const { TabPane } = Tabs;
+const { TabPane } = Tabs
 
 export default function HomePage() {
-  const formRef = useRef();
-  const [loading, setLoading] = useState(false);
-  const dispatch = useDispatch();
-  const { route } = useContext(RouterContext);
-  const authReducer = useSelector((state) => state.authReducer);
-  const navigate = useNavigate();
-  let location = useLocation();
-  let type = useNavigationType();
+  const formRef = useRef()
+  const [loading, setLoading] = useState(false)
+  const dispatch = useDispatch()
+  const { route } = useContext(RouterContext)
+  const authReducer = useSelector((state) => state.authReducer)
+  const navigate = useNavigate()
+  let location = useLocation()
+  let type = useNavigationType()
 
   useEffect(() => {
     if (route.to && authReducer.status) {
-      navigate(route.to);
+      navigate(route.to)
     }
-  }, []);
+  }, [])
 
   const onLogin = async (val) => {
-    setLoading(true);
+    setLoading(true)
     dispatch(AuthAction.AuthLogin(val))
-    setLoading(false);
-  };
-
+    setLoading(false)
+  }
+  const loginWithGoogle = async (val) => {
+    alert('Login with Google')
+  }
   const onRegister = async (val) => {
-    setLoading(true);
-    await dispatch(AuthAction.AuthRegister(val));
-    setLoading(false);
-  };
+    setLoading(true)
+    await dispatch(AuthAction.AuthRegister(val))
+    setLoading(false)
+  }
 
-
-  if (authReducer.status) {    // console.log(navigate, location, type);
+  if (authReducer.status) {
+    // console.log(navigate, location, type);
     // navigate(-1 || authReducer.role);
 
-    if (type !== "POP") {
-      navigate(-1);
+    if (type !== 'POP') {
+      navigate(-1)
     } else {
-      navigate(authReducer.role);
+      navigate(authReducer.role)
     }
   }
   return (
     <Tabs defaultActiveKey="1" centered>
       <TabPane tab="Đăng nhập" key="1">
-        <LoginForm ref={formRef} onFinish={onLogin} loading={loading} />;
+        <LoginForm
+          ref={formRef}
+          onFinish={onLogin}
+          loading={loading}
+          loginWithGoogle={loginWithGoogle}
+        />
       </TabPane>
       <TabPane tab="Đăng kí" key="2">
         <RegisterForm ref={formRef} onFinish={onRegister} loading={loading} />;
       </TabPane>
     </Tabs>
-  );
+  )
 }
