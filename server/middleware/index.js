98ab1f039e35544b85e03474dc84efa428fc4bb1
handwithfivefingers <handgod1995@gmail.com>
2022-10-28 15:30:22 +0700

    update ip whitelist

--- server/middleware/index.js
+++ server/middleware/index.js
@@ -70,8 +70,36 @@ const TrackingApi = async (req, res, next) => {
   }
 }
 
+const validateIPNVnpay = (req, res, next) => {
+  const SANDBOX_WHITE_LIST = ['113.160.92.202']
+  const PRODUCT_WHITE_LIST = [
+    '113.52.45.78',
+    '116.97.245.130',
+    '42.118.107.252',
+    '113.20.97.250',
+    '203.171.19.146',
+    '103.220.87.4',
+  ]
+
+  let validIps = [...SANDBOX_WHITE_LIST, ...PRODUCT_WHITE_LIST] // Put your IP whitelist in this array
+
+  if (validIps.includes(req.connection.remoteAddress)) {
+    // IP is ok, so go on
+    console.log('IP ok')
+    next()
+  } else {
+    // Invalid ip
+    console.log('Bad IP: ' + req.connection.remoteAddress)
+    const err = new Error('Bad IP: ' + req.connection.remoteAddress)
+    // next(err)
+
+    return res.status(403).json({ message: 'You are not allowed to access' })
+  }
+}
+
 module.exports = {
   upload,
   requireSignin,
   TrackingApi,
+  validateIPNVnpay,
 }
